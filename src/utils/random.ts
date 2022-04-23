a123899f09b87c666c734d63f21d2a4a23be1742
Aldo Preciado Cuevas <aldo@katulu.io>
2022-04-23 10:26:00 +0200

    refactor(random): use only random number instead or timestamp + random
    The random number generated for the id is now is only using the the generate random number function
    Before it was a combination of ${timestamp}{random4DigitNumber} so for every millisecond there could only be 9999 unique values.
    Now we can have 5,4 trillion different values out of the box (maximum number we can use for a 7-digit base 66 value)

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,31 +1,29 @@
-const BASE_64_CHARACTERS =
+const BASE_66_CHARACTERS =
   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-.~";
+const MAX_7_DIGIT_IN_BASE_64_BASE_10_NUMBER = 5455160701055; // equals ~~~~~~~ in our custom base 66
 
-const generate4DigitRandomNumber = (): number => {
-  const randomNumber = Math.random() * (1 - 999) + 1;
+const generateRandomInteger = (max = Number.MAX_SAFE_INTEGER) => {
+  const randomNumber = Math.random() * max;
 
   return Math.floor(randomNumber);
 };
 
 const decimalToBase66 = (number: number) => {
   let digits = "";
-  let finished = false;
   let currentQuotient = number;
 
-  while (!finished) {
-    digits += BASE_64_CHARACTERS[currentQuotient % 66];
+  do {
+    digits += BASE_66_CHARACTERS[currentQuotient % 66];
     currentQuotient = Math.floor(currentQuotient / 66);
-
-    if (currentQuotient === 0) {
-      finished = true;
-    }
-  }
+  } while (currentQuotient !== 0);
 
   return digits;
 };
 
 export const generateRandomBase66String = () => {
-  const randomTsNumber = `${Date.now()}${generate4DigitRandomNumber()}`;
+  const randomNumber = generateRandomInteger(
+    MAX_7_DIGIT_IN_BASE_64_BASE_10_NUMBER
+  );
 
-  return decimalToBase66(parseInt(randomTsNumber));
+  return decimalToBase66(randomNumber);
 };
