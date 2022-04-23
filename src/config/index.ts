6631bd1d017dbc50a29a3775f2d3d3e881af91c9
Aldo Preciado Cuevas <aldo@katulu.io>
2022-04-23 11:19:12 +0200

    refactor(config): use environment variables instead of hardcoded values for non dev

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,24 +1,31 @@
-let envConfig;
+import env from "env-var";
+import type { DynamoDB } from "aws-sdk";
 
-if (process.env.STAGE === "dev") {
-  envConfig = {
-    urlTableName: "url-table",
-    urlStatsTableName: "url-stats-table",
-    dynamodb: {
-      region: "localhost",
-      endpoint: "http://localhost:8000",
-      accessKeyId: "DEFAULT_ACCESS_KEY",
-      secretAccessKey: "DEFAULT_SECRET",
-    },
+type StageConfig = {
+  urlTableName: string;
+  urlStatsTableName: string;
+  dynamodb?: DynamoDB.Types.ClientConfiguration;
+};
+
+const stage = env.get("STAGE").default("dev").asString();
+
+const envConfig: StageConfig = {
+  urlTableName: env.get("URL_TABLE_NAME").required().asString(),
+  urlStatsTableName: env.get("URL_STATS_TABLE_NAME").required().asString(),
+};
+
+if (stage === "dev") {
+  envConfig.dynamodb = {
+    region: "localhost",
+    endpoint: "http://localhost:8000",
+    accessKeyId: "DEFAULT_ACCESS_KEY",
+    secretAccessKey: "DEFAULT_SECRET",
   };
-} else {
-  envConfig = {
-    urlTableName: "url-table",
-    urlStatsTableName: "url-stats-table",
-    dynamodb: {
-      region: "eu-central-1",
-    },
-  };
+  // serverless offline cannot reference resource return values so we have
+  // to hardcode the values for now, further investigation is needed
+  // Ref: https://github.com/dherault/serverless-offline/issues/198
+  envConfig.urlTableName = "url-table";
+  envConfig.urlStatsTableName = "url-stats-table";
 }
 
 export const config = envConfig;
